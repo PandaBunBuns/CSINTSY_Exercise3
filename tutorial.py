7cf2527e1fc0426d08ce315f06a1b639df476eea
PandaBunBuns <87511092+PandaBunBuns@users.noreply.github.com>
2023-10-13 16:25:46 +0800

    added tutorial

--- tutorial.py
+++ tutorial.py
@@ -261,3 +261,215 @@ def print_size_of_model(model):
 # 3. Define Dataset and Data Loaders #
 ######################################
 
+# ImageNet Data
+
+def prepare_data_loaders(data_path):
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+    dataset = torchvision.datasets.ImageNet(
+        data_path, split="train", transform=transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]))
+    dataset_test = torchvision.datasets.ImageNet(
+        data_path, split="val", transform=transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize,
+        ]))
+
+    train_sampler = torch.utils.data.RandomSampler(dataset)
+    test_sampler = torch.utils.data.SequentialSampler(dataset_test)
+
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=train_batch_size,
+        sampler=train_sampler)
+
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=eval_batch_size,
+        sampler=test_sampler)
+
+    return data_loader, data_loader_test
+
+# After downloading the pre-trained model
+
+data_path = '~/.data/imagenet'
+saved_model_dir = 'data/'
+float_model_file = 'mobilenet_pretrained_float.pth'
+scripted_float_model_file = 'mobilenet_quantization_scripted.pth'
+scripted_quantized_model_file = 'mobilenet_quantization_scripted_quantized.pth'
+
+train_batch_size = 30
+eval_batch_size = 50
+
+data_loader, data_loader_test = prepare_data_loaders(data_path)
+criterion = nn.CrossEntropyLoss()
+float_model = load_model(saved_model_dir + float_model_file).to('cpu')
+
+# Next, we'll "fuse modules"; this can both make the model faster by saving on memory access
+# while also improving numerical accuracy. While this can be used with any model, this is
+# especially common with quantized models.
+
+print('\n Inverted Residual Block: Before fusion \n\n', float_model.features[1].conv)
+float_model.eval()
+
+# Fuses modules
+float_model.fuse_model()
+
+# Note fusion of Conv+BN+Relu and Conv+Relu
+print('\n Inverted Residual Block: After fusion\n\n',float_model.features[1].conv)
+
+num_eval_batches = 1000
+
+print("Size of baseline model")
+print_size_of_model(float_model)
+
+top1, top5 = evaluate(float_model, criterion, data_loader_test, neval_batches=num_eval_batches)
+print('Evaluation accuracy on %d images, %2.2f'%(num_eval_batches * eval_batch_size, top1.avg))
+torch.jit.save(torch.jit.script(float_model), saved_model_dir + scripted_float_model_file)
+
+########################################
+# 4. Post-training static quantization #
+########################################
+
+num_calibration_batches = 32
+
+myModel = load_model(saved_model_dir + float_model_file).to('cpu')
+myModel.eval()
+
+# Fuse Conv, bn and relu
+myModel.fuse_model()
+
+# Specify quantization configurat
+# Start with simple min/max range estimation and per-tensor quantization of weights
+myModel.qconfig = torch.ao.quantization.default_qconfig
+print(myModel.qconfig)
+torch.ao.quantization.prepare(myModel, inplace=True)
+
+# Calibrate first
+print('Post Training Quantization Prepare: Inserting Observers')
+print('\n Inverted Residual Block:After observer insertion \n\n', myModel.features[1].conv)
+
+# Calibrate with the training set
+evaluate(myModel, criterion, data_loader, neval_batches=num_calibration_batches)
+print('Post Training Quantization: Calibration done')
+
+# Convert to quantized model
+torch.ao.quantization.convert(myModel, inplace=True)
+print('Post Training Quantization: Convert done')
+print('\n Inverted Residual Block: After fusion and quantization, note fused modules: \n\n',myModel.features[1].conv)
+
+print("Size of model after quantization")
+print_size_of_model(myModel)
+
+top1, top5 = evaluate(myModel, criterion, data_loader_test, neval_batches=num_eval_batches)
+print('Evaluation accuracy on %d images, %2.2f'%(num_eval_batches * eval_batch_size, top1.avg))
+
+
+per_channel_quantized_model = load_model(saved_model_dir + float_model_file)
+per_channel_quantized_model.eval()
+per_channel_quantized_model.fuse_model()
+# The old 'fbgemm' is still available but 'x86' is the recommended default.
+per_channel_quantized_model.qconfig = torch.ao.quantization.get_default_qconfig('x86')
+print(per_channel_quantized_model.qconfig)
+
+torch.ao.quantization.prepare(per_channel_quantized_model, inplace=True)
+evaluate(per_channel_quantized_model,criterion, data_loader, num_calibration_batches)
+torch.ao.quantization.convert(per_channel_quantized_model, inplace=True)
+top1, top5 = evaluate(per_channel_quantized_model, criterion, data_loader_test, neval_batches=num_eval_batches)
+print('Evaluation accuracy on %d images, %2.2f'%(num_eval_batches * eval_batch_size, top1.avg))
+torch.jit.save(torch.jit.script(per_channel_quantized_model), saved_model_dir + scripted_quantized_model_file)
+
+
+##################################
+# 5. Quantization-aware training #
+##################################
+
+def train_one_epoch(model, criterion, optimizer, data_loader, device, ntrain_batches):
+    model.train()
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    avgloss = AverageMeter('Loss', '1.5f')
+
+    cnt = 0
+    for image, target in data_loader:
+        start_time = time.time()
+        print('.', end = '')
+        cnt += 1
+        image, target = image.to(device), target.to(device)
+        output = model(image)
+        loss = criterion(output, target)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        top1.update(acc1[0], image.size(0))
+        top5.update(acc5[0], image.size(0))
+        avgloss.update(loss, image.size(0))
+        if cnt >= ntrain_batches:
+            print('Loss', avgloss.avg)
+
+            print('Training: * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+                  .format(top1=top1, top5=top5))
+            return
+
+    print('Full imagenet train set:  * Acc@1 {top1.global_avg:.3f} Acc@5 {top5.global_avg:.3f}'
+          .format(top1=top1, top5=top5))
+    return
+
+qat_model = load_model(saved_model_dir + float_model_file)
+qat_model.fuse_model()
+
+optimizer = torch.optim.SGD(qat_model.parameters(), lr = 0.0001)
+# The old 'fbgemm' is still available but 'x86' is the recommended default.
+qat_model.qconfig = torch.ao.quantization.get_default_qat_qconfig('x86')
+
+torch.ao.quantization.prepare_qat(qat_model, inplace=True)
+print('Inverted Residual Block: After preparation for QAT, note fake-quantization modules \n',qat_model.features[1].conv)
+
+num_train_batches = 20
+
+# QAT takes time and one needs to train over a few epochs.
+# Train and check accuracy after each epoch
+for nepoch in range(8):
+    train_one_epoch(qat_model, criterion, optimizer, data_loader, torch.device('cpu'), num_train_batches)
+    if nepoch > 3:
+        # Freeze quantizer parameters
+        qat_model.apply(torch.ao.quantization.disable_observer)
+    if nepoch > 2:
+        # Freeze batch norm mean and variance estimates
+        qat_model.apply(torch.nn.intrinsic.qat.freeze_bn_stats)
+
+    # Check the accuracy after each epoch
+    quantized_model = torch.ao.quantization.convert(qat_model.eval(), inplace=False)
+    quantized_model.eval()
+    top1, top5 = evaluate(quantized_model,criterion, data_loader_test, neval_batches=num_eval_batches)
+    print('Epoch %d :Evaluation accuracy on %d images, %2.2f'%(nepoch, num_eval_batches * eval_batch_size, top1.avg))
+
+    # SPEEDUP FROM QUNATIZATION
+
+    def run_benchmark(model_file, img_loader):
+    elapsed = 0
+    model = torch.jit.load(model_file)
+    model.eval()
+    num_batches = 5
+    # Run the scripted model on a few batches of images
+    for i, (images, target) in enumerate(img_loader):
+        if i < num_batches:
+            start = time.time()
+            output = model(images)
+            end = time.time()
+            elapsed = elapsed + (end-start)
+        else:
+            break
+    num_images = images.size()[0] * num_batches
+
+    print('Elapsed time: %3.0f ms' % (elapsed/num_images*1000))
+    return elapsed
+
+run_benchmark(saved_model_dir + scripted_float_model_file, data_loader_test)
+
+run_benchmark(saved_model_dir + scripted_quantized_model_file, data_loader_test)
